fd05eaf76cd5390ba5e061f82dbcb4d05060d500
TT <tt@gmail.com>
2025-06-03 20:56:37 +0700

    Học lập trình Java và thực hành Git

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+public class helloworld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Tự học Java cơ bản");
+        System.out.println("Học Java để làm gì?");
+    }
+}
